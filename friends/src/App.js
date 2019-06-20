12db87fad9ecf07eebefb7bc559897f0e058119c
David Quiñones <daquinons@users.noreply.github.com>
2019-06-20 16:08:35 +0200

    Cleanup App

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { login } from './state/actionCreators';
 import Header from './components/Header/Header';
 import AddEditFriend from './components/AddEditFriend/AddEditFriend';
@@ -8,26 +7,6 @@ import FriendsList from './components/FriendsList/FriendsList';
 import './App.css';
 
 function App(props) {
-  const [editableFriend, setEditableFriend] = useState(undefined);
-  const URL = 'http://localhost:5000/friends';
-
-  const addNewFriend = async friend => {
-    try {
-      await axios.post(URL, friend);
-      //await getFriends();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const chooseEditableFriend = friend => {
-    setEditableFriend(friend);
-  };
-
-  const cancelEdit = () => {
-    setEditableFriend(undefined);
-  };
-
   useEffect(() => {
     props.login('Lambda School', 'i<3Lambd4');
   }, [props]);
@@ -36,11 +15,8 @@ function App(props) {
     <div className="App">
       <Header />
       <AddEditFriend
-        onAddFriend={addNewFriend}
-        editableFriend={editableFriend}
       />
       <FriendsList
-        onClickEdit={chooseEditableFriend}
       />
     </div>
   );
