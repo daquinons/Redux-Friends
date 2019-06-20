d6620a0522daa380505214e17929b8e15032f860
David Quiñones <daquinons@users.noreply.github.com>
2019-06-20 13:57:24 +0200

    Add action types

---
+++ friends/src/state/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADD_FRIEND = 'ADD_FRIEND';
+export const GET_FRIENDS = 'GET_FRIENDS';
+export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const SET_EDITABLE_FRIEND = 'SET_EDITABLE_FRIEND';
+export const LOGIN = 'LOGIN';
